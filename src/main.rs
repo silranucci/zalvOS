ccb2eb9dbe7ba06fff38558c32a130c628464fe5
Silvestro Ranucci <sranu@pm.me>
2025-03-25 19:30:07 +0100

    feat: add vga driver

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,17 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello, world!";
-
 #[no_mangle] 
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    HELLO.iter().enumerate().for_each(|(i, &ch)| {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = ch;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    });
+    println!("Hello World{}", "!");
 
     loop {}
 }
